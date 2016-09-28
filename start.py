b6897d6c1631847d076dc4bc0cf2ccd17fa94cc1
fchui <masil@live.ca>
2016-09-28 01:44:26 -0700

    Added Rock Paper Scissors
    plus Easter Egg

--- start.py
+++ start.py
@@ -14,7 +14,7 @@ async def on_ready():
 
 @bot.command()
 async def rtd(min_number : str ,max_number: str):
-    """Rolls the Dice function, takes in integers"""
+    """ Rolls the Dice function, takes in integers """
     random.seed()
     
     try:
@@ -23,7 +23,47 @@ async def rtd(min_number : str ,max_number: str):
         await bot.say('Invalid Input, Please enter numbers only')
         return
 
-    await bot.say('You rolled ' + str(int(dice_number))) 
+    await bot.say('You rolled ' + str(int(dice_number)))
+
+@bot.command()
+async def rps(user_hand : str):
+    """ Rock Paper Scissors function, takes in string
+        Easter Egg: はさみ For User Scissors in JP """
+    random.seed()
+    
+    bot_choice = random.choice(["rock","paper","scissors"])
+   # try:
+    if user_hand == "rock" or user_hand == "paper" or user_hand == "scissors" or user_hand == "はさみ":
+        if bot_choice == user_hand:
+            await bot.say("Its a tie. It isn't like you won or anything! BAKA")
+                
+        elif bot_choice == "rock" and user_hand == "paper":
+            await bot.say("You win against Tsundere-chan's Rock. It's not like I let you win or anything BAKA!")
+        elif bot_choice == "rock" and user_hand == "scissors":
+            await bot.say("You lose against Tsundere-chan's Rock")
+                
+        elif bot_choice == "paper" and user_hand == "rock":
+             await bot.say("You lose against Tsundere-chan's Paper")
+        elif bot_choice == "paper" and user_hand == "scissors":
+             await bot.say("You win against Tsundere-chan's Paper. It's not like I let you win or anything BAKA!")
+                
+        elif bot_choice == "scissors" and user_hand == "rock":
+             await bot.say("You win against Tsundere-chan's Scissors. It's not like I let you win or anything BAKA!")
+                
+        elif bot_choice == "paper" and user_hand == "はさみ":
+             await bot.say("You win against Tsundere-chan's Paper baka weeb")
+        elif bot_choice == "rock" and user_hand == "はさみ":
+             await bot.say("You lose against Tsundere-chan's Rock baka weeb")
+                
+        else:
+             await bot.say ("You lose against Tsundere-chan's Scissors")
+    else:
+         await bot.say('Invalid input, I want rock, paper or scissors! BAKA')
+            
+                
+    #except Exception:
+    #    await bot.say('')
+    
 
 '''Bot execution using token'''
 bot.run('MTcxNTQ0NzI2MTU4MTgwMzU0.CsZ7hA.j8pERaIZY0SObQujX2C0Yyb8HIU')
