c6d770c86443babb660aca36f8e2e028b084398b
fchui <masil@live.ca>
2016-09-28 01:00:02 -0700

    Roll the Dice Added

--- start.py
+++ start.py
@@ -1,14 +1,16 @@
 import discord
-import asyncio
+from discord.ext import commands
+import random
 
-client = discord.Client()
+'''Command Prefix'''
+bot = commands.Bot(command_prefix='!')
 
-'''Called on client done preparing data, NOT FIRST always'''
-@client.event
+'''Called on bot done preparing data, NOT FIRST always'''
+@bot.event
 async def on_ready():
-    print('Logged in as: ' + client.user.name)
-    print('Bot ID: ' + client.user.id)
-    await client.change_status(game=discord.Game(name='with Rem')) 
+    print('Logged in as: ' + bot.user.name)
+    print('Bot ID: ' + bot.user.id)
+    await bot.change_status(game=discord.Game(name='with Rem')) 
 
 @client.event
 async def on_message(message):
@@ -16,7 +18,7 @@ async def on_message(message):
         await client.send_message(message.channel, 'you baka')
 
 '''Bot execution using token'''
-client.run('MTcxNTQ0NzI2MTU4MTgwMzU0.CsZ7hA.j8pERaIZY0SObQujX2C0Yyb8HIU')
+bot.run('MTcxNTQ0NzI2MTU4MTgwMzU0.CsZ7hA.j8pERaIZY0SObQujX2C0Yyb8HIU')
 
     
     
